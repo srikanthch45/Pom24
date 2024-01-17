49e1864077f295fb48c4563fdf95d14bac81da22
srikanth <srikanthchinthalapudi224@gmail.com>
2024-01-17 14:27:19 +0530

    added new methods into cart page

--- src/main/java/com/qa/Ae/pages/cartpage.java
+++ src/main/java/com/qa/Ae/pages/cartpage.java
@@ -1,5 +1,13 @@
 package com.qa.Ae.pages;
 
 public class cartpage {
+	
+	public void cart() {
+		System.out.println("added cart");
+	}
+	
+	public void addcart() {
+		System.out.println("add cart page");
+	}
 
 }
